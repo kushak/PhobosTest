55995579bd5af9bc923c41fdfe0db7e7a14ba528
kushak <ship93@yandex.ru>
2017-03-11 09:27:57 +0300

    Initial Commit

---
+++ PhobosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhobosTest
+//
+//  Created by user on 11.03.17.
+//  Copyright © 2017 Oleg Shipulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhobosTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhobosTest
+//
+//  Created by user on 11.03.17.
+//  Copyright © 2017 Oleg Shipulin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
